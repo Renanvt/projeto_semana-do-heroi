676e279fcef964c7eb55cbd3dc87dd61fe4a6ab3
Renanvt <jonatan_rvs@hotmail.com>
2024-06-04 15:24:33 -0300

    Adicionando a barra de pesquisa

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -32,11 +32,15 @@ export function Home() {
             <Header />
             <Container>
                 <Title title="O que você quer ler hoje?" />
-                <div className="gap-5 grid grid-cols-8">
+                <div className="gap-8 grid grid-cols-8 my-6">
                     {genderBooks.map((book) => (
                         <Button title={book} variant={selectedGender.includes(book) ? 'dark' : 'light'} 
                         onClick={()=>handleSelect(book)} />
                     ))}
+                </div>
+                <div className='pt-7'>
+                    <p className='text-evergreen font-semibold text-2xl'>Sobre o que você gostaria de receber uma recomendação de livro?</p>
+                    <input type="text" placeholder="Eu gostaria de ler..." className="outline-none shadow-lg border border-gray-100 rounded-lg w-full p-3 mt-3" />
                 </div>
             </Container>
         </>
